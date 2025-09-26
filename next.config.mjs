a3b108c5a8fd1c2fa060412b2026a85826e4fd22
Adam Qureshi <adamqureshi@gmail.com>
2025-09-26 14:02:07 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "i.imgur.com" },
+    ],
+  },
 };
+
 export default nextConfig;
